d93a1fdf3c4a88ef6df6c9678cd0d09dcd8bf74f
trickle404 <m.derkaqw@gmail.com>
2025-07-18 11:58:22 +0200

    readme : added image

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # scrapy_spider_task
-Test task: Scrape the first 150 proxies and save a file proxis.json
+## Test task: 
+
+1. Scrape the first 150 proxies and save a file proxis.json
+output.json structure:
+[
+{"ip": "0.0.0.0", "port": 8080, "protocols": ["HTTP", "HTTPS"]},
+{"ip": "1.1.1.1", "port": 3180, "protocols": ["SOCKS4", "SOCKS5"]}
+...
+]
+2. Submit the scraped proxies to the form, using the
+provided personal token in the "Personal token" field.
+output.json structure:
+results.json structure:
+{
+"save_id_1":["proxy","proxy","..."],
+"save_id_2":["proxy","proxy","..."],
+"save_id_n":["proxy","proxy","..."]
+}
+
+## Solution:
+1. 
+
+2. 
+
+![alt text](image.png)
--- proxis/proxis/spiders/proxis_spider.py
+++ proxis/proxis/spiders/proxis_spider.py
@@ -4,6 +4,7 @@ import base64
 class ProxisSpider(scrapy.Spider):
     name = "advanced_name"
     start_urls = ["https://advanced.name/freeproxy?page=1"]
+    api_urls = "https://test-rg8.ddns.net/api/post_proxies"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -11,9 +12,10 @@ class ProxisSpider(scrapy.Spider):
         self.limit = 150
         self.page = 1
 
+    #Парсим данные с странице advanced_name
     def parse(self, response):
         for i, row in enumerate(response.css('tbody tr'), 1):
-            if self.count > self.limit: return
+            if self.count >= self.limit: return
             self.count += 1
             yield {
                 "ip" : base64.b64decode(row.css('td[data-ip]::attr(data-ip)').get()).decode('utf-8'),
@@ -21,9 +23,17 @@ class ProxisSpider(scrapy.Spider):
                 "protocols": row.css('td:nth-child(4) a::text').getall()
             }
             
-
-        if self.count <= self.limit:
+        # Для обработки страниц. Если не использовать, то паук будет брать данные только с первой страницы. Их примерно 100
+        # По условию надо взять 150 прокси
+        if self.count < self.limit:
             self.page += 1
             next_page = f"https://advanced.name/freeproxy/?page={self.page}"
             yield response.follow(next_page, callback = self.parse)
+
+    #Загрузка данных на тестовое окружение
+    def upload_to_form(self, request):
+        i = 1
+        yield {
+            f"save_id_{i}" : ["n15"]
+        }
 #t_1363631d
